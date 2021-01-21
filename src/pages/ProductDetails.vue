af715d1bf76df85c410c8d393723bf6b88353639
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-01-21 16:15:51 -0400

    Routing con params dinamicos comp API
    - Para usar parametros dinamicos en los componentes, usamos props.
    - Props nos permitiran crear desde la funcion router en main.js valores dinamicos dentro de setup() composition API.
    - Para ser aun mas dinamicos podemos trabajar con computed properties function en setup composition API. (como en ProducDetails.vue).

--- src/pages/ProductDetails.vue
+++ src/pages/ProductDetails.vue
@@ -1,19 +1,28 @@
 <template>
   <section>
     <h2>{{ title }}</h2>
-    <h3>${{ price}}</h3>
-    <p>{{ description}}</p>
+    <h3>${{ price }}</h3>
+    <p>{{ description }}</p>
+    <router-link to="/products/p2">Product 2</router-link>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { inject, computed } from 'vue';
 
 export default {
-  setup() {
-    const title = ref('');
-    const price = ref(null);
-    const description = ref('');
+  props: ['pid'],
+  setup(props) {
+    const products = inject('products');
+
+    // ponemos products.value porque se injecta como ref (ver App.vue), por eso debemos extraer el valor.
+    const selectedProduct = computed(() =>
+      products.value.find((product) => product.id === props.pid)
+    );
+    // Se coloco el .value porque ahora selecteProducts es un ref (porque lo hicimos con computed property).
+    const title = computed(() => selectedProduct.value.title);
+    const price = computed(() => selectedProduct.value.price);
+    const description = computed(() => selectedProduct.value.description);
 
     return { title, price, description };
   },
